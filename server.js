8d504da56bc4d89655a2931e0a313965a9af88f3
Karimk <karimkapadia@gmail.com>
2020-11-18 17:02:21 -0700

    deploying

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false });
 
 app.put("/api/workouts/", (req, res) => {
   console.log(`put [/api/workouts]`,JSON.stringify(req.body));
